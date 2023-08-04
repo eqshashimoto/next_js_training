d92264fb4a48fe9d56d010f867974b2b7a92bf04
hashimoto-motchan <hashimoto20540@gmail.com>
2023-08-04 11:08:45 +0900

    練習

---
+++ nextjs-example/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    webpack: (config, { isServer }) => {
+        // 空のオブジェクト渡すことでnpmパッケージがfsモジュールに依存しないようにします
+        if (!isServer) {
+            config.node = {
+                fs: 'empty'
+            }
+        }
+        return config
+    }
+}
+
+
+
---
+++ nextjs-example/pages/index.js
@@ -0,0 +1,31 @@
+// import Link from "next/link";
+// import fetch from "isomorphic-unfetch";
+
+// const Index = props => (
+//   <div>
+//     <h1>Batman TV Shows</h1>
+//     <ul>
+//       {props.shows.map(show => (
+//         <li key={show.id}>
+//           <Link href="/shows/[id]" as={`/shows/${show.id}`}>
+//             <a>{show.name}</a>
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
+
+// Index.getInitialProps = async function() {
+//   const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+//   const data = await res.json();
+//   console.log(`Show data fetched. Count: ${data.length}`);
+//   return { shows: data.map(entry => entry.show) };
+// };
+
+// export default Index;
+
+const Index = () => {
+  return <h1>Hello World</h1>;
+};
+export default Index;
